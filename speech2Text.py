57c578277e25b4d6667972e70621674540be5973
Parth <parth.321.06@gmail.com>
2026-06-10 21:52:03 +0530

    Speech to text converter

---
+++ speech2Text.py
@@ -0,0 +1,27 @@
+import speech_recognition as sr
+import pyttsx3
+
+Voice = pyttsx3.init()
+
+def Speak(command):
+    Voice.say(command)
+    Voice.runAndWait()
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Adjusting ambient noise...")
+    r.adjust_for_ambient_noise(source, duration = 2)
+    print("Start Speaking...")
+    
+    audio2 = r.listen(source, timeout = 3)
+    
+    
+    try:        
+        textT = r.recognize_google(audio2)
+        textT = textT.lower()
+        print("Heard text: " + textT)
+        Speak(textT)
+    except sr.UnknownValueError:
+        print("unable to recognize speech")
+    except sr.RequestError:
+        print("unable to connect xxxxx")
